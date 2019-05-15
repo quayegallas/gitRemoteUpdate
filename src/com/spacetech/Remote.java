8641ec5fe6ae277d1c886db503e594b2b8de6274
lz <liuze@spacetech.com.cn>
2019-05-15 16:59:42 +0800

    自动获取git路径

--- src/com/spacetech/Remote.java
+++ src/com/spacetech/Remote.java
@@ -27,24 +27,27 @@ public class Remote extends AnAction {
 		BufferedReader reader = null;
 		Project project = e.getProject();
 		String url = project.getProjectFilePath();
-		String trueUrl = url.substring(0,url.indexOf("/.idea"));
+		String trueUrl = url.substring(0, url.indexOf("/.idea"));
+		String line = "";
 		try {
-			p = Runtime.getRuntime().exec("/usr/local/bin/git remote update origin -p", null,
-                    new File(trueUrl));
+			p = Runtime.getRuntime().exec("which git");
+			inputStreamReader = new InputStreamReader(p.getInputStream(), "UTF-8");
+			reader = new BufferedReader(inputStreamReader);
+			line = reader.readLine();
+			p = Runtime.getRuntime().exec(line + " remote update origin -p", null, new File(trueUrl));
 			p.waitFor();
 			inputStreamReader = new InputStreamReader(p.getInputStream(), "UTF-8");
 			reader = new BufferedReader(inputStreamReader);
-			String line = "";
 			while ((line = reader.readLine()) != null) {
 				output.append(line + "\n");
 			}
-            System.out.println(output.toString());
-            Messages.showMessageDialog("Success","Information", Messages.getInformationIcon());
-		} catch (InterruptedException ex) {
-			ex.printStackTrace();
+			System.out.println(output.toString());
+			Messages.showMessageDialog("正在更新..", "更新远端分支", Messages.getInformationIcon());
 		} catch (UnsupportedEncodingException ex) {
 			ex.printStackTrace();
 		} catch (IOException ex) {
+			ex.printStackTrace();
+		} catch (InterruptedException ex) {
 			ex.printStackTrace();
 		}
 	}
